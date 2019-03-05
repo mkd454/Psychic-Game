0effdb0b033b2dce9e5014089b3be627b3576901
Mary Dang <dangmaryk@gmail.com>
2019-03-04 20:57:00 -0600

    Majority of the game is done, but needs work on bugs to reset game & fix lose

---
+++ assets/javascript/game.js
@@ -0,0 +1,68 @@
+// Rules //
+
+// CPU picks a letter
+// Player guesses a letter
+// Player has 10 tries, if failure, track letters until no more guesses -> reset
+// Track score
+
+// CPU picks a letter from the alphabet array. Value gets stored in randomLetter variable
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+var randomLetter = getRandomLetter;
+
+// Score beginning values set
+var win = 0;
+var lose = 0;
+var guessesLeft = 9;
+var guessesDisplayed = "";
+var guess;
+
+// RESET VALUES
+function reset() {
+  getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+  guessesLeft = 9;
+  guess;
+  guessesDisplayed.length = 0;
+  console.log(randomLetter);
+}
+
+console.log(randomLetter);
+
+// Run whenever the user presses a key
+document.onkeyup = function(event) {
+  guess = event.key;
+
+  // Make sure the user input is valid
+  if (alphabet.indexOf(guess) >= 0 && guessesDisplayed.indexOf(guess) < 0) {
+    console.log("valid entry");
+    
+    // Win situation
+    if (guess === randomLetter) {
+      win++;
+      document.getElementById("win").innerHTML = win;
+      reset();
+    } // When guess is wrong, but still have guesses left
+    else if (guess !== randomLetter && guessesLeft > 0) {
+      guessesLeft--;
+      document.getElementById("guessesLeft").innerHTML = guessesLeft;
+      
+      if (guessesDisplayed === "") {
+        guessesDisplayed = guess;
+        document.getElementById("guessesDisplayed").innerHTML = guessesDisplayed;
+      } else {
+        guessesDisplayed = guessesDisplayed + ", " + guess;
+        document.getElementById("guessesDisplayed").innerHTML = guessesDisplayed;
+      }
+    } // If none of the above happen, it is safe to assume the user loses and the game resets
+    else {
+      lose++;
+      document.getElementById("lose").innerHTML = lose;
+      reset();
+    }
+  } 
+}
+
+
+
+
+
